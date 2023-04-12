89947b5b13d328d348a2cbc220cc13d921845c2a
Haritonov Vasilii <haritonov_vasya@inbox.ru>
2023-04-12 21:57:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,6 +11,5 @@ def solution(x_success: int,
              y_cnt: int) -> bool:
     
     alpha = 0.05
-    _, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
-    
+    _, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
     return False if p_value > alpha else True
